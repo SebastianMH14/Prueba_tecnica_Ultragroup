15e22b6f11fe4bbdcea36461d0f59b8727df3e2a
SebastianMH14 <sebaasmh2002@hotmail.com>
2023-03-05 17:42:27 -0500

    add postgres

--- app/conf/settings.py
+++ app/conf/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME', cast=str),
+        'USER': config('DB_USER', cast=str),
+        'PASSWORD': config('DB_PASSWORD', cast=str),
+        'HOST': config('DB_HOST', cast=str),
+        'PORT': '5510',
     }
 }
 
