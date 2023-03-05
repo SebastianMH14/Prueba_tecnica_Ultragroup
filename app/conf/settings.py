51799543793d4d9ca2ea174e0b4caf9672f6bc7c
SebastianMH14 <sebaasmh2002@hotmail.com>
2023-03-05 18:50:32 -0500

    fix static path

--- app/conf/settings.py
+++ app/conf/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =  '../app/static'
+STATIC_ROOT =  'static'
 print(STATIC_ROOT)
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'hotel_booking/static'),
